4cd62d1554ad79c57af5f7103e0712594b2e5c37
fkerich <festus.kerich@cellulant.io>
2022-03-10 20:12:44 +0300

    initial commit

---
+++ src/main/java/com/francium/tailwind/TailwindApplication.java
@@ -0,0 +1,13 @@
+package com.francium.tailwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailwindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TailwindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/francium/tailwind/TailwindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.francium.tailwind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailwindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
